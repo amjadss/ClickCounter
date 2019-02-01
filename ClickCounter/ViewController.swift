9106fb253a21877014303d844b1dc64645bbe7ad
Amjad Al-Sulaimani <amjad.ss@hotmail.com>
2019-02-01 11:07:42 +0300

    Click Counter project initialization

---
+++ ClickCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClickCounter
+//
+//  Created by Amjad Al-Sulaimani on 2019-02-01.
+//  Copyright © 2019 Amjad Sulaimani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
